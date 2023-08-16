11e4d888f8df046463c0bef5dded84ffb982e3b6
bubble <guyyan12@gmail.com>
2023-08-16 14:03:03 +0300

    Updated ditails

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+#1
+print(" Hello Enter your  name: Guy")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#2
+print(" Hello Enter your adress: hativat atzioni 27")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
